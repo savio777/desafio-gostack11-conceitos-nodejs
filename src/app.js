865095a6cad9bf88fbdca56aaab8ae9a1f5acaef
savio <saviopf22@gmail.com>
2020-08-20 00:46:26 -0300

    resposta do desafio de acordo com o que foi pedido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,66 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = { id: uuid(), title, url, techs: techs.split(", "), likes: 0 };
+
+  repositories.push(repo);
+
+  return response.json({ repo });
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const findIndex = repositories.findIndex((r) => r.id == id);
+  const findRepo = repositories.find((r) => r.id == id);
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs: techs.split(", "),
+    likes: findRepo.likes,
+  };
+
+  repositories[findIndex] = repo;
+
+  return response.json({ repo });
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findIndex = repositories.findIndex((r) => r.id == id);
+
+  repositories.splice(findIndex, 1);
+
+  return response.json({ deleted: id });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findIndex = repositories.findIndex((r) => r.id == id);
+  const findRepo = repositories.find((r) => r.id == id);
+
+  const repo = {
+    id,
+    title: findRepo.title,
+    url: findRepo.url,
+    techs: findRepo.techs,
+    likes: findRepo.likes + 1,
+  };
+
+  repositories[findIndex] = repo;
+
+  return response.json({ repo });
 });
 
 module.exports = app;
